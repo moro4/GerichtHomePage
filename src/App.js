bfc264876499653ab4544dcc0c92f842915360af
zepeersGily <>
2022-07-21 12:52:36 +0200

    implemented video section

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => (
       <AboutUs />
       <SpecialMenu />
       <Chef />
-      {/*<Intro />
-      <Laurels />
+      <Intro />
+      {/*<Laurels />
       <Gallery />
       <FindUs />
       <Footer /> */}
